5d5473b0c4807fd242a0b89c6eb693a3161a77ab
nates linux desktop <nhapeman@gmail.com>
2022-12-30 22:56:36 -0800

    Generalize most plotting.

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,25 @@ import cv2
 def load_rgb(path: str):
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
 
+def plot(rows, cols, *images: tuple[str, np.ndarray], figsize: None | tuple[int, int] = None):
+    """Generically plots images and their titles while removing the axis."""
+    # Single image.
+    if rows == cols == 1:
+        plt.title(images[0][0])
+        plt.imshow(images[0][1])
+        plt.axis("off")
+        return
+    # Many images.
+    f, axs = plt.subplots(rows, cols, figsize=figsize)
+    for index, (title, image) in enumerate(images):
+        if image.ndim == 2:
+            axs[index].imshow(image, cmap="gray")
+        else:
+            axs[index].imshow(image)
+        axs[index].axis('off')
+        axs[index].title.set_text(title)
+    f.tight_layout()
+
 def save_gray(garr: np.ndarray, name: str):
     """Saves a 2D grayscale image."""
     image2 = Image.fromarray(np.uint8(garr), "L")
@@ -15,8 +34,10 @@ def save_gray(garr: np.ndarray, name: str):
     image2.save(f"generated/{name}")
 
 
-def adjust_gamma(image: np.ndarray, gamma=1.0):
-    """Adjust an image's gamma using a look up table."""
+def adjust_exponential(image: np.ndarray, gamma=1.0):
+    """Adjust an array exponentially using a look up table.
+
+    This is a very handy function for things like gamma correction."""
     invGamma = 1.0 / gamma
     float_table = [((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]
     table = np.array(float_table).astype("uint8")
